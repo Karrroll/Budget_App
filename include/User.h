74ed54d6276d867f9a43cbaa62c329c7e4b35802
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-05-15 12:08:09 +0200

    Add user structure to headers file

---
+++ include/User.h
@@ -0,0 +1,12 @@
+#ifndef USER_H_INCLUDED
+#define USER_H_INCLUDED
+
+Struct User {
+    int id = 0;
+    string firstName = "";
+    string lastName = "";
+    string userName = "";
+    string userPassword = "";
+};
+
+#endif // USER_H_INCLUDED
