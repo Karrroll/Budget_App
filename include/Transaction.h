0ee7808e2bddb15c11889544e9dee79a8a47cff3
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-05-15 12:19:45 +0200

    Add transaction structure to headers file

---
+++ include/Transaction.h
@@ -0,0 +1,12 @@
+#ifndef TRANSACTION_H_INCLUDED
+#define TRANSACTION_H_INCLUDED
+
+Struct Transaction {
+    int transactionId = 0;
+    int userId = 0;
+    int date = 0;
+    string item = "";
+    double amount = 0.0;
+};
+
+#endif // TRANSACTION_H_INCLUDED
