ea8738c8d7f4ca80e79093dc1611196626ae9a71
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-05-24 14:56:10 +0200

    Create Type class with enums for field types

---
+++ include/Type.h
@@ -0,0 +1,11 @@
+#ifndef TYPE_H_INCLUDED
+#define TYPE_H_INCLUDED
+
+enum class FieldType {
+    FIRST_NAME,
+    LAST_NAME,
+    LOGIN,
+    PASSWORD
+};
+
+#endif // TYPE_H_INCLUDED
--- src/UserManager.cpp
+++ src/UserManager.cpp
@@ -1,10 +1,22 @@
 #include "UserManager.h"
 
 User UserManager::enterUserData() {
+    User user;
+    string firstName = "", lastName = "", userName = "", userPassword = "";
 
+//    user.id = file.getLastId()  // implementackja
+    cout << "First Name: ";
+    user.firstName = firstName;
+    cout << "Last Name: ";
+    user.lastName = lastName;
+    cout << "Login: ";
+    user.userName = userName;
+    cout << "Password: ";
+    user.userPassword = userPassword;
+
+    return user;
 }
 
 void UserManager::registerNewUser() {
 
 }
-
