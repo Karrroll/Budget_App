24795a1ee871a380a7cc446ce2cfc9be99b1ed9d
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-06-15 21:21:09 +0200

    Add methods to get, check, and convert date

--- include/DateMethods.h
+++ include/DateMethods.h
@@ -5,12 +5,20 @@
 #include <sstream>
 #include <ctime>
 #include <iomanip>
+#include <cctype>
+#include <conio.h>
 
 using namespace std;
 
 class DateMethods {
 public:
+    bool validateDate(const string &date);
+    bool isYearLeap(const int year);
+    bool isDataFormatValid(const string &date);
+    bool isDateRangeValid(const string &date);
     string getTodayDate();
+    string getUserSelectedDate();
+    int convertStringDateToInt(const string &dateStr);
 protected:
 
 private:
--- src/DateMethods.cpp
+++ src/DateMethods.cpp
@@ -1,37 +1,123 @@
 #include "DateMethods.h"
 
+bool DateMethods::validateDate(const string &date) {
+    return (isDataFormatValid(date) && isDateRangeValid(date));
+}
+
+bool DateMethods::isYearLeap(const int year) {
+    return ((year%4 == 0 && year%100 != 0) || (year%400 == 0));
+}
+
+bool DateMethods::isDataFormatValid(const string &date) {
+    if (date.length() == 8) {
+        for (char sign: date) {
+            if (!isdigit(sign))
+                return false;
+        }
+        return true;
+    }
+
+    cout << "Invalid date format." << endl;
+    system("pause");
+    return false;
+}
+
+bool DateMethods::isDateRangeValid(const string &date) {
+    const int MIN_YEAR = 2000;
+
+    int year = stoi(date.substr(0,4));
+    int month = stoi(date.substr(4,2));
+    int day = stoi(date.substr(6));
+    string currentDate = getTodayDate();
+
+    if (year < MIN_YEAR || date > currentDate) {
+        cout << "Invalid date. Date must be between 2000-01-01 and today. Try again." << endl;
+        system("pause");
+        return false;
+    }
+
+    if (month < 1 || month > 12) {
+        cout << "Invalid date. Month is out of range for the selected date. Try again." << endl;
+        system("pause");
+        return false;
+    }
+
+    int maxDaysInMonth = 31;
+    switch(month) {
+        case 4: case 6: case 9: case 11:
+            maxDaysInMonth = 30;
+            break;
+        case 2:
+            if (isYearLeap(year))
+                maxDaysInMonth = 29;
+            else
+                maxDaysInMonth = 28;
+            break;
+    }
+
+    if (day < 1 || day > maxDaysInMonth) {
+        cout << "Invalid date. Day is out of range for the selected date. Try again." << endl;
+        system("pause");
+        return false;
+    }
+
+    return true;
+}
+
 string DateMethods::getTodayDate() {
-    string currentDateString = "";
     ostringstream todayDate;
 
     time_t currentTime = time(0);
     tm *date = localtime(&currentTime);
 
-    todayDate << setw(4) << date->tm_year + 1900 << setw(2) << setfill('0') << date->tm_mon + 1 << setw(2) << setfill('0') << date->tm_mday;
+    todayDate << setfill('0') << setw(4) << date->tm_year + 1900 << setw(2) << date->tm_mon + 1 << setw(2) << date->tm_mday;
 
-    cout << todayDate.str() << endl;
-    return currentDateString;
+    return todayDate.str();
 }
 
+string DateMethods::getUserSelectedDate() {
+    string dateTemplate = "yyyy-mm-dd";
+    string userDate = dateTemplate;
+    string userDateStr = "";
 
+    cout << "Enter date (" << dateTemplate <<  "): " << endl;
 
-time_t aktualnyCzas = time(0);      //pobiera czas w sekundach od 1970 (0) czyli tylko zwraca nie zapisuje do wskaŸnika
+    size_t i = 0;
+    while (i < userDate.length()) {
+        if (userDate[i] == '-') {
+            i++;
+            continue;
+        }
 
-tm *data = localtime(&aktualnyCzas);        // rozbija na strukture czasu lokalnego lokaltime oczekuje wskaŸnik i zapisuje do wkaŸnika tm przez co mam do niego potem dostêp w innych metodach aby wyci¹gn¹c date/godzine itd
+        cout << "\r" << userDate << flush;
+        char input = getch();
 
-cout << data -> tm_year << endl;
-cout << data -> tm_mon << endl;
-cout << data -> tm_mday << endl;
-cout << data -> tm_hour << endl;
-cout << data -> tm_min << endl;
-cout << data -> tm_sec << endl;
-cout << data -> tm_isdst << endl;
-cout << data -> tm_wday << endl;
-cout << data -> tm_yday << endl;        // wszytsko to int
+        if (input == 8 && i > 0) {
+            do {
+                i--;
+            } while (i > 0 && userDate[i] == '-');
 
+            userDate[i] = dateTemplate[i];
+            userDateStr.pop_back();
+            continue;
+        }
 
-ostringstream dzisiejszaData;
+        if (!isdigit(input))
+            continue;
 
-dzisiejszaData << setw(4) << setfill('0') << data -> tm_year + 1900 << "-" << setw(2) << setfill('0') << data -> tm_mon + 1 << "-" << setw(2) << setfill('0') << data -> tm_mday;
+        userDate[i] = input;
+        userDateStr += input;
+        i++;
+    }
+    cout<<endl;
 
-cout << dzisiejszaData.str() << endl;
+    if (!validateDate(userDateStr))
+        userDateStr.clear();
+
+    system("cls");
+    return userDateStr;
+}
+
+int DateMethods::convertStringDateToInt(const string &dateStr) {
+    return stoi(dateStr);
+}
