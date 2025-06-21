40c17f777c23438d73d5644c43f39674a9c25a70
Karol Jurzec <karol.jurzec.programista@gmail.com>
2025-06-22 00:03:01 +0200

    While loops instead of if statements and compact switch cases (main.cpp)

--- main.cpp
+++ main.cpp
@@ -9,18 +9,14 @@ int main() {
     BudgetMainApp budgetMainApp("users.xml", "incomes.xml", "expenses.xml");
 
     while (true) {
-        if (!budgetMainApp.isUserLoggedIn()) {
+        while (!budgetMainApp.isUserLoggedIn()) {
 
             Menus::showMenu(MenuType::MAIN_MENU);
             char userChoice = Utils::readCharacter();
 
             switch (userChoice) {
-                case '1':
-                    budgetMainApp.registerNewUser();
-                    break;
-                case '2':
-                    budgetMainApp.loginUser();
-                    break;
+                case '1': budgetMainApp.registerNewUser();  break;
+                case '2': budgetMainApp.loginUser();        break;
                 case '9':
                     cout << "Thank you for using the application. See you next time!" << endl;
                     system("pause");
@@ -32,32 +28,18 @@ int main() {
             }
         }
 
-        if (budgetMainApp.isUserLoggedIn()) {
+        while (budgetMainApp.isUserLoggedIn()) {
             Menus::showMenu(MenuType::USER_MENU);
             char userChoice = Utils::readCharacter();
 
             switch (userChoice) {
-                case '1':
-                    budgetMainApp.addIncome();
-                    break;
-                case '2':
-                    budgetMainApp.addExpense();
-                    break;
-                case '3':
-                    budgetMainApp.showCurrentMonthBalance();
-                    break;
-                case '4':
-                    budgetMainApp.showPreviousMonthBalance();
-                    break;
-                case '5':
-                    budgetMainApp.showCustomPeriodBalance();
-                    break;
-                case '6':
-                    budgetMainApp.changeUserPassword();
-                    break;
-                case '7':
-                    budgetMainApp.logoutUser();
-                    break;
+                case '1': budgetMainApp.addIncome();                break;
+                case '2': budgetMainApp.addExpense();               break;
+                case '3': budgetMainApp.showCurrentMonthBalance();  break;
+                case '4': budgetMainApp.showPreviousMonthBalance(); break;
+                case '5': budgetMainApp.showCustomPeriodBalance();  break;
+                case '6': budgetMainApp.changeUserPassword();       break;
+                case '7': budgetMainApp.logoutUser();               break;
                 default:
                     cout << "Invalid choice. Please try again." << endl;
                     system("pause");
